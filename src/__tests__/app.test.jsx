971dd38e6f6b835f5a36281a410d1b30f964824f
Peter Velkov <kidroca@gmail.com>
2017-12-20 16:21:27 +0200

    chore(app): add webpack starter kit

---
+++ src/__tests__/app.test.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import app from 'app';
+
+configure({ adapter: new Adapter() });
+
+describe('app', () => {
+
+  it('renders without crashing', () => {
+
+    mount(<app />);
+
+  });
+
+});
